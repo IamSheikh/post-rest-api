5cb31c9a2451a3e7d712b22a99ab0eca0c44caf6
TahiJameel <tahajameel.co@gmail.com>
2021-09-19 10:37:18 +0500

    add all routes in try catch block

--- routes/post.js
+++ routes/post.js
@@ -3,42 +3,62 @@ const post = require("./../models/post");
 const router = express.Router();
 
 router.post("/new", async (req, res) => {
-  const { username, title, description } = req.body;
-  const newPost = new post({
-    username: username,
-    title: title,
-    description: description,
-  });
-  const posts = await newPost.save();
-  res.json(posts);
+  try {
+    const { username, title, description } = req.body;
+    const newPost = new post({
+      username: username,
+      title: title,
+      description: description,
+    });
+    const posts = await newPost.save();
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.get("/search/:id", async (req, res) => {
-  const { id } = req.params;
-  const findedPost = await post.findById({ _id: id });
-  res.json(findedPost);
+  try {
+    const { id } = req.params;
+    const findedPost = await post.findById({ _id: id });
+    res.json(findedPost);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.get("/all", async (req, res) => {
-  const allPost = await post.find();
-  res.json(allPost);
+  try {
+    const allPost = await post.find();
+    res.json(allPost);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.put("/edit/:id", async (req, res) => {
-  const { id } = req.params;
-  const { title, description } = req.body;
-  const updatedPostData = {
-    title,
-    description,
-  };
-  const editPost = await post.findByIdAndUpdate(id, updatedPostData);
-  res.json(editPost);
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+    const updatedPostData = {
+      title,
+      description,
+    };
+    const editPost = await post.findByIdAndUpdate(id, updatedPostData);
+    res.json(editPost);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.delete("/delete/:id", async (req, res) => {
-  const { id } = req.params;
-  const deletePost = await post.findByIdAndDelete(id);
-  res.json(deletePost);
+  try {
+    const { id } = req.params;
+    const deletePost = await post.findByIdAndDelete(id);
+    res.json(deletePost);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
