e6792e54f932019aaea218ed9c9c8cef2931abe4
TahiJameel <tahajameel.co@gmail.com>
2021-09-19 09:35:06 +0500

    create express server and connect to mongodb

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/post-rest-api");
+    console.log("database connected");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+connectDB();
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Backend server running on http://localhost:${PORT}`);
+});
